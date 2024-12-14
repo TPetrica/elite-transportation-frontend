e37ee24ec36549e62924ed02bc3bb82b26383a57
TirlaP <tirla.petru@yahoo.com>
2024-12-14 12:59:26 +0200

    Resolve popper problem

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,26 @@
 /* eslint-disable no-undef */
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	build: {
+		rollupOptions: {
+			external: ["@popperjs/core"],
+			output: {
+				globals: {
+					"@popperjs/core": "Popper",
+				},
+			},
+		},
+	},
+	optimizeDeps: {
+		include: ["@popperjs/core"],
+	},
 });
