f7daa588683a537cc5b12dae15297be88ff6f636
TirlaP <tirla.petru@yahoo.com>
2025-01-10 18:49:50 +0100

    Solve popper.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,6 @@
 import { AuthProvider } from "@/context/AuthContext.jsx";
+import "@popperjs/core";
+import "bootstrap";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
